d1420e74f80327bd04e9d0dc4ef63a82d1b5d2ec
Mzee1991 <yohanamariamzee@gmail.com>
2022-05-31 20:34:37 +0300

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 def print_uniq_combos():
-    for i in range(0, 9):
-        for j in range(i + 1, 10):
-            if i == 8 and j == 9:
-                print("{:d}{:d}".format(i, j))
-            else:
-                print("{:d}{:d}".format(i, j), end=", ")
-
-
-if __name__ == "__main__":
-    print_uniq_combos()
+    number = 0
+    while number <= 89:
+        if number % 10 == 0:
+            number += 1 + number // 10
+        print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+        number += 1
