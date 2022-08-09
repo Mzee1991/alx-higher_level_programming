f3e21f6dbdc0d6fc1bed5fe53a39cd2e046e229e
Mzee1991 <yohanamariamzee@gmail.com>
2022-08-09 11:32:28 -0700

    creat and remove database, listing tables

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creates the database in the MySQL server
+-- ifit doesn't already exists
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- deletes the database from the server if the database exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
