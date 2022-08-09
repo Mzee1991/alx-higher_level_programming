2f88d2e88c5bc17d59febe9b2f996ef3afb0ed9c
Mzee1991 <yohanamariamzee@gmail.com>
2022-08-09 12:29:49 -0700

    task 10, 11, and 12

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table "second_table"
+-- Results shld display both the score and the name(in this order)
+-- Records shld be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
